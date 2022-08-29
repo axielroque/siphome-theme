61fcf9e457d695a58a7480c87c046546dd7efbdc
Axiel Roque <axiel.roque@outlook.es>
2022-08-29 11:03:46 -0600

    added color to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,29 @@ module.exports = {
         '2xl': '1600px',
       },
     },
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#fff',
+        solidBlack: '#262B35',
+        blue: '#1C57C9',
+        green: '#1ECE7A',
+        gray: '#606C84',
+        'slate': {
+          50: '#f8fafc',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+          400: '#94a3b8',
+        },
+        'tahiti': {
+          light: '#67e8f9',
+          DEFAULT: '#06b6d4',
+          dark: '#0e7490',
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
